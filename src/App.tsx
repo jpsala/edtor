6e31c3b1c9315d5b642ed7869fa5fa2eeaece55b
Juan Pablo Sala <jpsala@gmail.com>
2023-08-22 17:23:12 -0300

    First

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="">
+        <textarea style={{minHeight: '99vh', minWidth: '100%'}}/>
+      </div>
+    </>
+  )
+}
+
+export default App
